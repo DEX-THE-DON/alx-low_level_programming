699849a8c9c1dcccdefd25bd0a62d2d08a8cdfe1
DEX-THE-DON <dennisdexthedon@gmail.com>
2023-03-16 08:19:18 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * return: always 0
  */
 
-int main()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
