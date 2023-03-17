ac147de7af14515ae74bb86fc03bd0955e5b25e0
DEX-THE-DON <dennisdexthedon@gmail.com>
2023-03-17 16:03:48 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,46 @@
 #include <stdlib.h>
 
 /**
+ * main - Entry #include <stdio.h>
+ * #include <stdlib.h>
+ *
+ */**
  * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
+ *  * Ret#include <stdio.h>
+ *  #include <stdlib.h>
+ *
+ */**
+  * main - Entry point
+  *  * Retur#include <stdio.h>
+  *  #include <stdlib.h>
+  *
+  */**
+   * main - Entry point
+   *  * Return:#include <stdio.h>
+   *  #include <stdlib.h>
+   *
+   */**
+    * main - Entry point
+    *  * Return: A#include <stdio.h>
+    *  #include <stdlib.h>
+    *
+    */**
+     * main - Entry point
+     *  * Return: Alw#include <stdio.h>
+     *  #include <stdlib.h>
+     *
+     */**
+      * main - Entry point
+      *  * Return: Alway#include <stdio.h>
+      *  #include <stdlib.h>
+      *
+      */**
+       * main - Entry point
+       *  * Return: Always 1 (Success)
+       *   */
+       int main(void)
 {
-fprintf(stderr,	"and that piece of art is useful\
-- Dora Korpar, 2015-10-19\n");
-return (1);
+		write(STDOUT_FILENO, "and that piece of art is useful
+							\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
