3c4d2939891c7892ba1a24df216b79e385d1d64c
DEX-THE-DON <dennisdexthedon@gmail.com>
2023-03-17 10:21:29 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char dex;
+
+	for (dex = 'z'; dex >= 'a'; dex--)
+		putchar(dex);
+		putchar('\n');
+
+	return (0);
+}
