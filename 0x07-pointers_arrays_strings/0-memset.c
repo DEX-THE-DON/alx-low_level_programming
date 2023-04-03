dfcd3d68b6bccc60ea40fbd990b40e7b8c9639ec
DEX-THE-DON <dennisdexthedon@gmail.com>
2023-04-03 04:33:31 -0700

    Write a function that fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *start = s;
+
+	while (n--)
+	{
+		*s = b;
+		s++;
+	}
+	return (start);
+}
